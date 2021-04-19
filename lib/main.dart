687ab47559329f21b4d76018670963d005f13222
Arman <javad.zahmatkesh@gmail.com>
2021-04-19 16:45:11 +0300

    button and edit added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:erpui/module/consts.dart';
+import 'package:erpui/module/widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -27,98 +28,87 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-          child: Row(children: [
-        // Align(
-        //     alignment: Alignment.centerLeft,
-        Expanded(
-            flex: 1,
-            child: Container(
-              // color: Colors.blue.shade900,
-              decoration: BoxDecoration(
+        child: Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Container(
+                // color: Colors.blue.shade900,
+                decoration: BoxDecoration(
                   gradient: LinearGradient(
-                colors: [
-                  Colors.blue.shade200,
-                  Colors.blue.shade400,
-                  Colors.blue.shade700,
-                  Colors.blue.shade900,
-                ],
-                begin: Alignment.topRight,
-                end: Alignment.bottomLeft,
-              )),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Text(
-                    'Wellcome',
-                    style: TextStyle(fontSize: 38, color: Colors.white),
-                  ),
-                  Container(
-                    height: screenHeight(context) * 0.7,
-                    width: screenWidth(context) * 0.3,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage('images/background.png'),
-                          fit: BoxFit.cover),
-                    ),
+                    colors: [
+                      Colors.blue.shade200,
+                      Colors.blue.shade400,
+                      Colors.blue.shade700,
+                      Colors.blue.shade900,
+                    ],
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
                   )
-                ],
-              ),
-            )),
-        // Align(
-        //     alignment: Alignment.centerRight,
-        Expanded(
-            // margin: EdgeInsets.only(right: screenWidth(context) * 0.10),
-            child: Container(
-                margin: EdgeInsets.symmetric(
-                    horizontal: screenWidth(context) * 0.15),
-                child: Card(
-                    elevation: 11,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                    child: Container(
-                      // decoration: BoxDecoration(
-                      //     color: Colors.grey.shade100,
-                      //     borderRadius: BorderRadius.circular(12)),
-                      padding: EdgeInsets.all(18),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          CircleAvatar(
-                            radius: 50,
-                            backgroundImage:
-                                NetworkImage('http://i.imgur.com/ryybk8P.jpg'),
-                          ),
-                          SizedBox(height: 35),
-                          TextFormField(
-                            autofocus: true,
-                            decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(12)),
-                                labelText: 'username'),
-                          ),
-                          SizedBox(height: 10),
-                          TextFormField(
-                            obscureText: true,
-                            decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(12)),
-                                labelText: 'password'),
-                          ),
-                          SizedBox(height: 25),
-                          Row(
-                            children: [
-                              ElevatedButton(
-                                  child: Text('Login'), onPressed: () {}),
-                              SizedBox(width: 10),
-                              TextButton(
-                                  onPressed: () {},
-                                  child: Text('forgot password?'))
-                            ],
-                          )
-                        ],
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      'Welcome',
+                      style: TextStyle(
+                        fontFamily: 'pacifico',
+                        fontSize: 78, 
+                        color: Colors.white70
                       ),
-                    )))),
+                    ),
+                    Container(
+                      height: screenHeight(context) * 0.7,
+                      width: screenWidth(context) * 0.3,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('images/background.png'),
+                            fit: BoxFit.cover),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ),
+            Expanded(
+                child: Container(
+                  margin: EdgeInsets.symmetric(
+                      horizontal: screenWidth(context) * 0.15),
+                  child: Card(
+                      elevation: 11,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      child: Container(
+                        padding: EdgeInsets.all(18),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            CircleAvatar(
+                              radius: 50,
+                              backgroundImage:
+                                  NetworkImage('http://i.imgur.com/ryybk8P.jpg'),
+                            ),
+                            SizedBox(height: 35),
+                            EditBox(label: 'username'),
+                            EditBox(label: 'password', password: true),
+                            SizedBox(height: 25),
+                            Row(
+                              children: [
+                                Button(
+                                  caption: 'Login',
+                                  icon: Icon(Icons.lock_outline),
+                                  onPressed: () {}
+                                ),
+                                TxtButton(
+                                  onPressed: () {},
+                                  caption: 'forgot password?'
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      )))),
       ])),
     );
   }
---
+++ lib/module/widget.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+enum EditType { Text, Number, Money, Date }
+
+class MoneyTextInputFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    if (newValue.text.length == 0) {
+      return newValue.copyWith(text: '');
+    } else if (newValue.text.compareTo(oldValue.text) != 0) {
+      int selectionIndexFromTheRight =
+          newValue.text.length - newValue.selection.extentOffset;
+      List<String> chars = newValue.text.replaceAll(',', '').split('');
+      String newString = '';
+      for (int i = 0; i < chars.length; i++) {
+        if (i % 3 == 0 && i != 0) newString += ',';
+        newString += chars[i];
+      }
+
+      return TextEditingValue(
+        text: newString,
+        selection: TextSelection.collapsed(
+          offset: newString.length - selectionIndexFromTheRight,
+        ),
+      );
+    } else {
+      return newValue;
+    }
+  }
+}
+
+class DateTextInputFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    if (newValue.text.length == 0) {
+      return newValue.copyWith(text: '');
+    } else if (newValue.text.compareTo(oldValue.text) != 0) {
+      int selectionIndexFromTheRight =
+          newValue.text.length - newValue.selection.extentOffset;
+      List<String> chars = newValue.text.replaceAll('/', '').split('');
+      String newString = '';
+      for (int i = 0; i < chars.length; i++) {
+        if (i == 4 || i == 6) newString += '/';
+        newString += chars[i];
+      }
+
+      return TextEditingValue(
+        text: newString,
+        selection: TextSelection.collapsed(
+          offset: newString.length - selectionIndexFromTheRight,
+        ),
+      );
+    } else {
+      return newValue;
+    }
+  }
+}
+
+class EditBox extends StatelessWidget {
+  final String label;
+  final String? defaultvalue;
+  final bool expanded;
+  final bool password;
+  final void Function(String)? onChange;
+  final TextEditingController? controller;
+  final EditType type;
+  final int? maxLength;
+
+  EditBox(
+      {required this.label,
+      this.defaultvalue,
+      this.expanded = false,
+      this.password = false,
+      this.onChange,
+      this.controller,
+      this.type = EditType.Text,
+      this.maxLength});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: EdgeInsets.all(4),
+        child: this.expanded ? Expanded(child: builder()) : builder());
+  }
+
+  Widget builder() {
+    return TextFormField(
+      decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+          labelText: this.label,
+          counterText: this.type == EditType.Date ? '' : null),
+      initialValue: this.defaultvalue,
+      obscureText: this.password,
+      onChanged: this.onChange,
+      maxLength: this.type == EditType.Date ? 10 : this.maxLength,
+      keyboardType: [EditType.Money, EditType.Number].contains(this.type)
+          ? TextInputType.number
+          : TextInputType.text,
+      inputFormatters: [
+        [EditType.Number, EditType.Money, EditType.Date].contains(this.type)
+            ? FilteringTextInputFormatter.digitsOnly
+            : FilteringTextInputFormatter.deny(''),
+        this.type == EditType.Money
+            ? MoneyTextInputFormatter()
+            : FilteringTextInputFormatter.deny(''),
+        this.type == EditType.Date
+            ? DateTextInputFormatter()
+            : FilteringTextInputFormatter.deny('')
+      ],
+    );
+  }
+}
+
+class Button extends StatelessWidget {
+  
+  final String caption;
+  final void Function() onPressed;
+  final Icon? icon;
+
+  Button({required this.caption, required this.onPressed, this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(4),
+      child: ElevatedButton(
+        onPressed: this.onPressed, 
+        child: this.icon == null
+          ? Text('${this.caption}')
+          : Row(
+            children: [
+              this.icon!,
+              SizedBox(width: 5),
+              Text('${this.caption}')
+            ],
+          ),
+        style: ButtonStyle(
+          padding: MaterialStateProperty.resolveWith((states) => EdgeInsets.symmetric(horizontal: 28, vertical: 18))
+        ),
+      ),
+    );
+  }
+}
+
+class TxtButton extends StatelessWidget {
+  
+  final String caption;
+  final void Function() onPressed;
+
+  TxtButton({required this.caption, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(4),
+      child: TextButton(onPressed: this.onPressed, child: Text('${this.caption}'))
+    );
+  }
+}
+
